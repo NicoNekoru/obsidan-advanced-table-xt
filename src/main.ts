60ce64ec8ab003f10d28725dcdec3d9fc959cdbf
NicoNekoru <66227158+NicoNekoru@users.noreply.github.com>
2023-08-11 18:37:00 -0600

    remove react artifact

---
+++ src/main.ts
@@ -0,0 +1,59 @@
+import { MarkdownPostProcessorContext, Plugin, MarkdownPreviewRenderer, htmlToMarkdown } from 'obsidian';
+// import { SheetSettingsTab } from './settings';
+import { SheetElement } from './sheetElement';
+// Remember to rename these classes and interfaces!
+
+export class ObsidianSpreadsheet extends Plugin 
+{
+	async onload() 
+	{
+		console.log('loading spreadsheet plugin');
+		this.registerMarkdownCodeBlockProcessor(
+			'sheet',
+			async (
+				source: string,
+				el: HTMLElement,
+				ctx: MarkdownPostProcessorContext
+			) => 
+			{
+				source = (new DOMParser)
+					.parseFromString(source.trim(), 'text/html')
+					.documentElement
+					.textContent || source.trim();
+					
+				ctx.addChild(new SheetElement(
+					el, 
+					source, 
+					ctx, 
+					this.app, 
+					this
+				));
+			}
+		);
+
+
+		MarkdownPreviewRenderer.registerPostProcessor(async (el, ctx) => 
+		{
+			if (!el.querySelector('table')) return;
+
+			if (el.querySelector('table')?.id === 'obsidian-sheets-parsed') return;
+
+			const source = htmlToMarkdown(el);			
+			if (!source) return;
+			
+			el.empty();
+			ctx.addChild(new SheetElement(el, source.trim(), ctx, this.app, this));
+		});
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		// this.addSettingTab(new SheetSettingsTab(this.app, this));
+	}
+
+	onunload() 
+	{
+		console.log('unloading spreadsheet plugin');
+	}
+
+}
+
+export default ObsidianSpreadsheet;
