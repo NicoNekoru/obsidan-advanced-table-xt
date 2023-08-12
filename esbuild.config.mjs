cc1e79629fa56646f3549431ba697c7607196c04
NicoNekoru <66227158+NicoNekoru@users.noreply.github.com>
2023-08-11 18:28:56 -0600

    Fixed an issue where table was unable to render in reading mode. Table still loses formatting in reading mode - still need to fix

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -15,7 +15,7 @@ const context = await esbuild.context({
 	banner: {
 		js: banner,
 	},
-	entryPoints: ['src/main.tsx'],
+	entryPoints: ['src/main.ts'],
 	bundle: true,
 	external: [
 		'obsidian',
